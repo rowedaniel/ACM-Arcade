9ff2967cafb0be031f455ee47c45c0fa0e98e29f
ACM <acmboard@lclark.edu>
2025-07-21 18:55:53 -0700

    Improve look of pico8 on CRT

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 # Boot PICO8 in carts/ directory
 source .env
 cd $PICO8_ROOT
-$PICO8 -splore -home ./home -root_path ./carts
+$PICO8 -splore -home ./home -root_path ./carts -windowed 0 -draw_rect 80,0,480,480
