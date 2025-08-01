bf825517cc32a700abf1c108fb37e1b81d95f450
ACM <acmboard@lclark.edu>
2025-07-31 17:27:32 -0700

    Remove logging code from run.py

--- run.py
+++ run.py
@@ -2,36 +2,27 @@ import subprocess
 import os
 import signal
 import time
-import logging
 from inputs import get_gamepad
 import threading
 
 START_BTN = "BTN_BASE4"
 
-logging.basicConfig(filename="screensaver.log", level=logging.INFO)
-logger = logging.getLogger()
-
 def get_winid(pid):
     return subprocess.run(['xdotool', 'search', '--pid',  str(pid)], capture_output=True).stdout.strip()
 
 def focus_window(winid, otherid):
-    logger.info(f'switching to, {winid}, from {otherid}. time is {time.time()}')
     subprocess.run(['xdotool', 'windowmap', winid.decode('utf-8')])
     if otherid:
         subprocess.run(['xdotool', 'windowunmap', otherid.decode('utf-8')])
 
 def start_program(cmd):
-    try:
-        prog_pid = subprocess.Popen(cmd, stdout=subprocess.PIPE, preexec_fn=os.setsid)
-        pico_pid = int(prog_pid.stdout.readline().strip())
-        logger.info(f'got pid: {pico_pid}')
-        p_winid = None
-        while not p_winid:
-            time.sleep(0.1)
-            p_winid = get_winid(pico_pid)
-        return prog_pid, p_winid
-    except Exception as e:
-        logger.info(f'got error: {e}')
+    prog_pid = subprocess.Popen(cmd, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+    pico_pid = int(prog_pid.stdout.readline().strip())
+    p_winid = None
+    while not p_winid:
+        time.sleep(0.1)
+        p_winid = get_winid(pico_pid)
+    return prog_pid, p_winid
 
 def start_screensaver(game_winid):
     screensaver, screensaver_winid = start_program(['./screensaver.sh'])
@@ -50,12 +41,7 @@ def main():
     time.sleep(4)
 
     # start game
-    logger.info('starting game')
     game,game_winid = start_program(['./startup.sh'])
-
-    logger.info(f'game:{game_winid}')
-
-
 
     # main loop
     idletime = time.time()
@@ -66,10 +52,8 @@ def main():
     pause_pressed = threading.Event()
     # make a thread for monitoring button inputs
     def read_events():
-        logger.info('starting thread')
         while True:
             for event in get_gamepad():
-                logger.info(f'got event {event}')
                 if event.code == START_BTN:
                     # in the screensaver, someone tried to pause
                     pause_pressed.set()
@@ -90,12 +74,10 @@ def main():
     focus_window(game_winid, None)
     while True:
         if in_game and time.time() - idletime > IDLETHRESH:
-            logger.info("in game, switching to screensaver")
             screensaver, screensaver_winid = start_screensaver(game_winid)
             in_game = False
         if poll_gamepad():
             if not in_game:
-                logger.info("in screensaver, switching to game")
                 resume_game(screensaver, screensaver_winid, game_winid)
                 in_game = True
             idletime = time.time()
